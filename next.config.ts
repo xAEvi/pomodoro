06b83f7c34e7fdd07621c0e07529aaf81743efc3
xAEvi <xeavi02@hotmail.com>
2026-07-15 11:06:41 -0300

    revert static export config for Vercel deployment

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export",
-  images: {
-    unoptimized: true,
-  },
-  basePath: "/pomodoro",
-};
+const nextConfig = {};
 
 export default nextConfig;
