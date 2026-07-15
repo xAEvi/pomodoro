f81579ff958c677def012a6e53b4de4cf3418cda
xAEvi <xeavi02@hotmail.com>
2026-07-15 11:00:49 -0300

    changed basePath

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export", // Fuerza la generación de HTML/CSS/JS estáticos en la carpeta /out
+  output: "export",
   images: {
-    unoptimized: true, // Requerido por Next.js para exportaciones estáticas
+    unoptimized: true,
   },
-  // ¡IMPORTANTE!: Si tu repositorio de GitHub NO es el principal de tu perfil
-  // (es decir, tu URL va a ser de tipo usuario.github.io/nombre-del-repo),
-  // debés descomentar la siguiente línea y poner el nombre de tu repositorio:
-  // basePath: '/nombre-de-tu-repositorio',
+  // Reemplazá 'pomodoro' por el nombre exacto de tu repositorio en GitHub
+  basePath: "/pomodoro",
 };
 
 export default nextConfig;
